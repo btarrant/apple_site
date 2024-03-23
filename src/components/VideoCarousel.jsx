5b820d9669dbd883ff16af4e0b6ce40cda4fb115
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-22 20:08:58 -0400

    Fix highlighSlides export in constants file

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { highlightsSlides } from "../constants";
 
 const VideoCarousel = () => {
-  return <div>VideoCarousel</div>;
+  return (
+    <>
+      <div className="flex items-center">
+        {highlightsSlides.map((list, i) => (
+          <div key={list.id} id="slider">
+            <div className="video-carousel_container">Test</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default VideoCarousel;
