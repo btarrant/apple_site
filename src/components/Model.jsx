50c0c23af6f480ae27586b1d312a32581a222fa5
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-22 21:00:29 -0400

    Install Three fiber and set up for 3D model

---
+++ src/components/Model.jsx
@@ -0,0 +1,48 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useState } from "react";
+import { yellowImg } from "../utils";
+import * as THREE from "three";
+
+const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    img: yellowImg,
+  });
+
+  // camera control for model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  // model
+  const small = useReff(new THREE.Group());
+  const large = useReff(new THREE.Group());
+
+  // rotation
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
+  useGSAP(() => {
+    gsap.to("#heading", { y: 0, opacity: 1 });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
