2681291ff9749b8962fec034111d7a4be0f0c3a2
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-24 00:10:58 -0400

    Reformat feature component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,7 @@
 import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
 import { animateWithGsap } from "../utils/animations";
-import { explore1Img, explore2Img, exploreVideo } from "../utils/index";
-import { useRef } from "react";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
 import gsap from "gsap";
 
 const Features = () => {
@@ -18,28 +18,27 @@ const Features = () => {
         videoRef.current.play();
       },
     });
-  });
 
-  useGSAP(() => {
     animateWithGsap("#features_title", { y: 0, opacity: 1 });
     animateWithGsap(".g_grow", { scale: 1, opacity: 1, ease: "power1" }, { scrub: 5.5 });
     animateWithGsap(".g_text", { y: 0, opacity: 1, ease: "power2.inOut", duration: 1 });
   }, []);
 
   return (
-    <section className=" h-full common-padding bg-zinc relative overflow-hidden">
-      <div className="screen-max-width">
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+      <div className="screen-max-wdith">
         <div className="mb-12 w-full">
           <h1 id="features_title" className="section-heading">
-            {" "}
             Explore the full story.
           </h1>
         </div>
+
         <div className="flex flex-col justify-center items-center overflow-hidden">
           <div className="mt-32 mb-24 pl-24">
             <h2 className="text-5xl lg:text-7xl font-semibold">iPhone.</h2>
             <h2 className="text-5xl lg:text-7xl font-semibold">Forged in titanium.</h2>
           </div>
+
           <div className="flex-center flex-col sm:px-10">
             <div className="relative h-[50vh] w-full flex items-center">
               <video
@@ -54,15 +53,17 @@ const Features = () => {
                 <source src={exploreVideo} type="video/mp4" />
               </video>
             </div>
+
             <div className="flex flex-col w-full relative">
               <div className="feature-video-container">
                 <div className="overflow-hidden flex-1 h-[50vh]">
                   <img src={explore1Img} alt="titanium" className="feature-video g_grow" />
                 </div>
                 <div className="overflow-hidden flex-1 h-[50vh]">
-                  <img src={explore2Img} alt="titanium2" className="feature-video g_grow" />
+                  <img src={explore2Img} alt="titanium 2" className="feature-video g_grow" />
                 </div>
               </div>
+
               <div className="feature-text-container">
                 <div className="flex-1 flex-center">
                   <p className="feature-text g_text">
@@ -73,11 +74,12 @@ const Features = () => {
                     , using the same alloy that spacecrafts use for missions to Mars.
                   </p>
                 </div>
+
                 <div className="flex-1 flex-center">
                   <p className="feature-text g_text">
-                    Tiatanium has one of the best strength-to-weight ratios of any metal, making
-                    these our <span className="text-white">lightest Pro models ever</span>. You'll
-                    notice the difference the moment you pick one up.
+                    Titanium has one of the best strength-to-weight ratios of any metal, making
+                    these our <span className="text-white">lightest Pro models ever.</span>
+                    You'll notice the difference the moment you pick one up.
                   </p>
                 </div>
               </div>
